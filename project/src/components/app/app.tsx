611532ff5e64e2fff9b881c1ade13e50da9ebcb6
AndreyAh <38111898+AndreyAh@users.noreply.github.com>
2023-03-06 20:45:43 +0700

    Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppScreenProps = {
+  NumberOfOffers: number;
+}
+
+function App({NumberOfOffers}: AppScreenProps): JSX.Element {
+  return (
+    <MainPage NumberOfOffers={NumberOfOffers} />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  NumberOfOffers: 3312,
+} as const;
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App NumberOfOffers = {Setting.NumberOfOffers} />
   </React.StrictMode>,
 );
